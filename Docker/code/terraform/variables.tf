7fd259f0655c3e32b73c697236f70dc64b825db8
Jesús Macias Portela <jesus.maciasportela@telefonica.com>
2018-12-18 14:03:11 +0100

    Update config

--- Docker/code/terraform/variables.tf
+++ Docker/code/terraform/variables.tf
@@ -1,3 +1,3 @@
-variable "endpoint_url" { default = "http://opennebula.cloud:2633/RPC2" }
-variable "one_username" { default = "user" }
-variable "one_password" { default = "password" }
+variable "endpoint_url" { default = "http://192.168.82.111:2633/RPC2" }
+variable "one_username" { default = "oneadmin" }
+variable "one_password" { default = "xxxxxxxx" }
